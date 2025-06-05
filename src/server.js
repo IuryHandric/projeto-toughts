359f098ffd584eac9c5f6aaca082da009273549a
Iury Handric <handric11@hotmail.com>
2025-06-05 09:37:03 -0300

    Criação dos Models Users e Toughts

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ const app = express();
 
 const conn = require('./db/conn')
 
+// Models
+
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
 // Template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -64,6 +69,7 @@ app.use((req, res, next) => {
 
 const port = process.env.DB_PORT || 3000
 conn
+    // .sync({force: true})
     .sync()
     .then(() => {
         app.listen(port, () => {
