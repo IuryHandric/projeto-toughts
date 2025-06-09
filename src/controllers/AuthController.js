0efc887f45f7e283592ca073724f2d2496891360
Iury Handric <handric11@hotmail.com>
2025-06-09 10:58:46 -0300

    Implementando flash message

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,8 +1,26 @@
+// Import do model User
+const User = require('../models/User')
+// Import do bcrypt para criptografar as senhas
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
     }
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body;
+
+        // Validação da senha
+        if(password != confirmpassword){
+            // message
+            req.flash('message', 'As senhas não estão iguais, tente novamente!')
+            res.render('auth/register')
+
+            return;
+        }
     }
 }
