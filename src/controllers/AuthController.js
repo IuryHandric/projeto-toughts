d706277586b71594f3df7af2f1fab10fc8d65907
Iury Handric <handric11@hotmail.com>
2025-06-09 11:40:16 -0300

    Adicionando usuário no banco de dados

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -22,5 +22,42 @@ module.exports = class AuthController {
 
             return;
         }
+
+        // Checando se o email já existe
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+
+        if(checkIfUserExists){
+            req.flash('message', 'O e-mail já está em uso!')
+            res.render('auth/register')
+
+            return
+        }
+
+
+        // Criando a senha com bcrypt
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        // Definindo que password passa a ser hashedPassword
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user)  
+
+            // Inicializando a sessão
+            req.session.userId = createdUser.id
+            req.flash('message', 'Usuário cadastrado com sucesso!')
+            req.session.save(() => {
+            })
+            res.redirect('/')
+            console.log(`Usuário ${name}, cadastrado com sucesso!`)     
+        } catch (e) {
+            console.log('Erro ao enviar', e)
+        }
+
+
     }
 }
