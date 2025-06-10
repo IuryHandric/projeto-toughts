99cbf0e2bfa332b9b1c29117b0c73543c0c370b7
Iury Handric <handric11@hotmail.com>
2025-06-10 10:20:58 -0300

    Mostrando pensamentos no dashboard

--- src/controllers/ToughtController.js
+++ src/controllers/ToughtController.js
@@ -8,7 +8,28 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+
+        const userId = req.session.userId;
+
+        const user = await User.findOne({
+            where: {
+                id: userId},
+                // Se trouxer o usuário, aqui dessa forma eu trago todos os pensamentos dele com o include
+                include: Tought,
+                // plain para trazer os melhores dados para manipulação
+                plain: true
+        })
+
+        // Verificando se Usuário existe
+        if(!user) {
+            res.redirect('/login')
+        }
+        // Map no array para utilização apenas dos valores necessários
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        console.log(toughts)
+
+        res.render('toughts/dashboard', {toughts})
     }
 
     static createTought(req, res) {
@@ -25,7 +46,7 @@ module.exports = class ToughtController {
             UserId: req.session.userId
         }
         // Verificação se foi enviado um texto e se foi passado apenas espaços em branco também.
-        if(!tought.title || tought.title.trim() === '') {
+        if (!tought.title || tought.title.trim() === '') {
             req.flash('message', 'Digite um pensamento antes de enviar!')
             return res.redirect('add')
         }
